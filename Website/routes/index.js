751ace82ca2e039372d14da383cd42cf685dd9e9
Jacob <jaa134@case.edu>
2019-04-07 20:40:50 -0400

    adds json portion of project to server

--- Website/routes/index.js
+++ Website/routes/index.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var jsonxml = require('jsontoxml');
+var https = require('https');
 
 router.get('/', function(req, res) {
   res.render('index', { title: 'CWRUded' });
 });
 
 router.get('/api/triviaJSON', function(req, res) {
-  res.status(200).json([]);
+  https.get('https://opentdb.com/api.php?amount=20&category=18', (resp) => {
+    let data = '';
+    resp.on('data', (chunk) => {
+      data += chunk;
+    });
+    resp.on('end', () => {
+      res.status(200).json(JSON.parse(data));
+    });
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+    res.status(200).json([]);
+  });
 });
 
 router.get('/api/triviaXML', function(req, res) {
